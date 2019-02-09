39d5c5d512d7cbac2776666b697fce8918f64bfb
Dankage102 <36318218+Dankage102@users.noreply.github.com>
2019-02-09 15:18:13 +0000

    Added :fire: emoji to active servers

--- bot1.js
+++ bot1.js
@@ -65,13 +65,13 @@ async function status(){
 	embed = new Discord.RichEmbed()
 	.setAuthor(client.user.username,client.user.avatarURL)
 	.setColor(Math.floor(Math.random() * 16777214) + 1)
-	.addField(":flag_gb:**Midgard [CTF] #1**","**Address**: <soldat://51.68.213.93:23081> \n **Players:**  `" + data1[0] + "/" + data1[1] + "`<:crouch:533700465670619197> **Map:**  `" + data1[2] + "`:map:")
-	.addField(":flag_de:**Midgard [CTF] #2**","**Address**: <soldat://138.201.55.232:25660> \n **Players:**  `" + data2[0] + "/" + data2[1] + "`<:crouch:533700465670619197> **Map:**  `" + data2[2] + "`:map:")
-	.addField(":flag_us:**Midgard [Final Bout]**","**Address**: <soldat://162.221.187.210:25020/003> \n **Players:**  `" + data3[0] + "/" + data3[1] + "`<:crouch:533700465670619197> **Map:**  `" + data3[2] + "`:map:")
-	.addField(":flag_us:**Midgard [HTF]**","**Address**: <soldat://162.221.187.210:25000> \n **Players:**  `" + data4[0] + "/" + data4[1] + "`<:crouch:533700465670619197> **Map:**  `" + data4[2] + "`:map:")
-	.addField(":flag_gb:**Midgard [Run Mode]**","**Address**: <soldat://51.68.213.93:23080> \n **Players:**  `" + data5[0] + "/" + data5[1] + "`<:crouch:533700465670619197> **Map:**  `" + data5[2] + "`:map:")
-	.addField(":flag_gb:**Midgard [Climb]**","**Address**: <soldat://51.68.213.93:23082> \n **Players:**  `" + data6[0] + "/" + data6[1] + "`<:crouch:533700465670619197> **Map:**  `" + data6[2] + "`:map:")
-	.addField(":flag_gb:**Midgard [AlphaZRPG]**","**Address**: <soldat://51.68.213.93:23083> \n **Players:**  `" + data7[0] + "/" + data7[1] + "`<:crouch:533700465670619197> **Map:**  `" + data7[2] + "`:map:")
+	.addField(":flag_gb:**Midgard [CTF] #1**","**Address**: <soldat://51.68.213.93:23081> \n"+(data1[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data1[0] + "/" + data1[1] + "`<:crouch:533700465670619197> **Map:**  `" + data1[2] + "`:map:")
+	.addField(":flag_de:**Midgard [CTF] #2**","**Address**: <soldat://138.201.55.232:25660> \n"+(data2[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data2[0] + "/" + data2[1] + "`<:crouch:533700465670619197> **Map:**  `" + data2[2] + "`:map:")
+	.addField(":flag_us:**Midgard [Final Bout]**","**Address**: <soldat://162.221.187.210:25020/003> \n"+(data3[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data3[0] + "/" + data3[1] + "`<:crouch:533700465670619197> **Map:**  `" + data3[2] + "`:map:")
+	.addField(":flag_us:**Midgard [HTF]**","**Address**: <soldat://162.221.187.210:25000> \n"+(data4[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data4[0] + "/" + data4[1] + "`<:crouch:533700465670619197> **Map:**  `" + data4[2] + "`:map:")
+	.addField(":flag_gb:**Midgard [Run Mode]**","**Address**: <soldat://51.68.213.93:23080> \n"+(data5[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data5[0] + "/" + data5[1] + "`<:crouch:533700465670619197> **Map:**  `" + data5[2] + "`:map:")
+	.addField(":flag_gb:**Midgard [Climb]**","**Address**: <soldat://51.68.213.93:23082> \n"+(data6[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data6[0] + "/" + data6[1] + "`<:crouch:533700465670619197> **Map:**  `" + data6[2] + "`:map:")
+	.addField(":flag_gb:**Midgard [AlphaZRPG]**","**Address**: <soldat://51.68.213.93:23083> \n"+(data7[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data7[0] + "/" + data7[1] + "`<:crouch:533700465670619197> **Map:**  `" + data7[2] + "`:map:")
 	.setTimestamp(new Date());
 	await client.channels.get(`537011480973934592`).send(embed);
 }
@@ -81,5 +81,3 @@ async function update(){
 	client.channels.get(`537011480973934592`).bulkDelete(fetched);
 	status();
 }
-
-
