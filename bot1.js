25d9ce87317d179b3b1444ece0532b3778d8967a
ShellModule <47338205+ShellModule@users.noreply.github.com>
2019-02-09 19:08:32 +0100

    Add files via upload

--- bot1.js
+++ bot1.js
@@ -36,42 +36,42 @@ async function status(){
 	await snekfetch.get(api[0]).then(r =>{
 		let body = r.body;
 		data1=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	}).then(
+	}).catch(err => data1=["N/A","N/A","N/A"]);
 	await snekfetch.get(api[1]).then(r =>{
 		let body = r.body;
 		data2=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	})).then(
+	}).catch(err => data2=["N/A","N/A","N/A"]);
 	await snekfetch.get(api[2]).then(r =>{
 		let body = r.body;
 		data3=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	})).then(
+	}).catch(err => data3=["N/A","N/A","N/A"]);
 	await snekfetch.get(api[3]).then(r =>{
 		let body = r.body;
 		data4=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	})).then(
+	}).catch(err => data4=["N/A","N/A","N/A"]);
 	await snekfetch.get(api[4]).then(r =>{
 		let body = r.body;
 		data5=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	})).then(
+	}).catch(err => data5=["N/A","N/A","N/A"]);
 	await snekfetch.get(api[5]).then(r =>{
 		let body = r.body;
 		data6=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	})).then(
+	}).catch(err => data6=["N/A","N/A","N/A"]);
 	await snekfetch.get(api[6]).then(r =>{
 		let body = r.body;
 		data7=[body.NumPlayers, body.MaxPlayers, body.CurrentMap];  
-	}));
+	}).catch(err => data7=["N/A","N/A","N/A"]);
 	
 	embed = new Discord.RichEmbed()
 	.setAuthor(client.user.username,client.user.avatarURL)
 	.setColor(Math.floor(Math.random() * 16777214) + 1)
-	.addField(":flag_gb:**Midgard [CTF] #1**","**Address**: <soldat://51.68.213.93:23081> \n"+(data1[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data1[0] + "/" + data1[1] + "`<:crouch:533700465670619197> **Map:**  `" + data1[2] + "`:map:")
-	.addField(":flag_de:**Midgard [CTF] #2**","**Address**: <soldat://138.201.55.232:25660> \n"+(data2[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data2[0] + "/" + data2[1] + "`<:crouch:533700465670619197> **Map:**  `" + data2[2] + "`:map:")
-	.addField(":flag_us:**Midgard [Final Bout]**","**Address**: <soldat://162.221.187.210:25020/003> \n"+(data3[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data3[0] + "/" + data3[1] + "`<:crouch:533700465670619197> **Map:**  `" + data3[2] + "`:map:")
-	.addField(":flag_us:**Midgard [HTF]**","**Address**: <soldat://162.221.187.210:25000> \n"+(data4[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data4[0] + "/" + data4[1] + "`<:crouch:533700465670619197> **Map:**  `" + data4[2] + "`:map:")
-	.addField(":flag_gb:**Midgard [Run Mode]**","**Address**: <soldat://51.68.213.93:23080> \n"+(data5[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data5[0] + "/" + data5[1] + "`<:crouch:533700465670619197> **Map:**  `" + data5[2] + "`:map:")
-	.addField(":flag_gb:**Midgard [Climb]**","**Address**: <soldat://51.68.213.93:23082> \n"+(data6[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data6[0] + "/" + data6[1] + "`<:crouch:533700465670619197> **Map:**  `" + data6[2] + "`:map:")
-	.addField(":flag_gb:**Midgard [AlphaZRPG]**","**Address**: <soldat://51.68.213.93:23083> \n"+(data7[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data7[0] + "/" + data7[1] + "`<:crouch:533700465670619197> **Map:**  `" + data7[2] + "`:map:")
+	.addField(":flag_gb:**Midgard [CTF] #1**",data1[0]==="N/A"?"N/A":"**Address**: <soldat://51.68.213.93:23081> \n"+(data1[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data1[0] + "/" + data1[1] + "`<:crouch:533700465670619197> **Map:**  `" + data1[2] + "`:map:")
+	.addField(":flag_de:**Midgard [CTF] #2**",data2[0]==="N/A"?"N/A":"**Address**: <soldat://138.201.55.232:25660> \n"+(data2[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data2[0] + "/" + data2[1] + "`<:crouch:533700465670619197> **Map:**  `" + data2[2] + "`:map:")
+	.addField(":flag_us:**Midgard [Final Bout]**",data3[0]==="N/A"?"N/A":"**Address**: <soldat://162.221.187.210:25020/003> \n"+(data3[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data3[0] + "/" + data3[1] + "`<:crouch:533700465670619197> **Map:**  `" + data3[2] + "`:map:")
+	.addField(":flag_us:**Midgard [HTF]**",data4[0]==="N/A"?"N/A":"**Address**: <soldat://162.221.187.210:25000> \n"+(data4[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data4[0] + "/" + data4[1] + "`<:crouch:533700465670619197> **Map:**  `" + data4[2] + "`:map:")
+	.addField(":flag_gb:**Midgard [Run Mode]**",data5[0]==="N/A"?"N/A":"**Address**: <soldat://51.68.213.93:23080> \n"+(data5[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data5[0] + "/" + data5[1] + "`<:crouch:533700465670619197> **Map:**  `" + data5[2] + "`:map:")
+	.addField(":flag_gb:**Midgard [Climb]**",data6[0]==="N/A"?"N/A":"**Address**: <soldat://51.68.213.93:23082> \n"+(data6[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data6[0] + "/" + data6[1] + "`<:crouch:533700465670619197> **Map:**  `" + data6[2] + "`:map:")
+	.addField(":flag_gb:**Midgard [AlphaZRPG]**",data7[0]==="N/A"?"N/A":"**Address**: <soldat://51.68.213.93:23083> \n"+(data7[0] > 0 ? ":fire: " : "")+" **Players:**  `" + data7[0] + "/" + data7[1] + "`<:crouch:533700465670619197> **Map:**  `" + data7[2] + "`:map:")
 	.setTimestamp(new Date());
 	await client.channels.get(`537011480973934592`).send(embed);
 }
